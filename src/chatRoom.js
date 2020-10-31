7f3cc4d6c56ed220ed7651548030229f193e4b6d
Aguilera4 <sergioaguileraramirez98@gmail.com>
2020-10-31 12:00:08 +0100

    Crear comentario - [HU11] #13

--- src/chatRoom.js
+++ src/chatRoom.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose');
+class ChatRoom {
 
-const chatRoomSchema = new mongoose.Schema({
-    id_room: Number,
-    comment_text: String,
-    name_user: String,
-    date_create: Date,
-})
+    // Constructor de la lase
+    constructor(id_room,name_user,comment_text,date_created){
+        this.id_room = id_room;
+        this.name_user = name_user;
+        this.comment_text = comment_text;
+        this.date_create = date_created;
+    }
 
-module.exports = mongoose.model('ChatRoom', chatRoomSchema)
+    // Devuelve el nombre del usuario
+    getNameUser(){
+        return this.name_user;
+    }
+
+    // Devuelve el comentario
+    getComment(){
+        return this.comment_text;
+    }
+
+    // Modificar el contenido del comentario
+    modifyComment(new_comment){
+        this.comment_text = new_comment;
+    }
+}
