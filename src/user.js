3c9b5618c65d46032bd6426b3f9f9660a829fde7
Aguilera4 <sergioaguileraramirez98@gmail.com>
2020-10-31 11:57:29 +0100

    Acceso del usuario - [HU1] #10

--- src/user.js
+++ src/user.js
@@ -1,11 +1,32 @@
-const mongoose = require('mongoose');
+class User {
+ 
+    // Constructor de la clase
+    constructor(name,email,city,password,tlfn,age){
+        this.name = name;
+        this.email = email;
+        this.city = city;
+        this.password = password;
+        this.tlfn = tlfn;
+        this.age = age;
+    }
 
-const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    city: String,
-    password: String,
-    tlfn: String
-})
+    // Cambio de email
+    changeEmail(new_email){
+        this.email = new_email;
+    }
 
-module.exports = mongoose.model('User', UserSchema)
+    // Obtener email
+    getEmail(){
+        return this.email;
+    }
+
+    // Obtener telefono
+    getTlfn(){
+        return this.tlfn;
+    }
+
+    // Obtener edad
+    getAge(){
+        return this.age;
+    }
+}
