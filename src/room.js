f585e95040428583aeeb3be3ad6941b6f9e38852
Aguilera4 <sergioaguileraramirez98@gmail.com>
2020-11-04 18:50:33 +0100

    Creación por parte de un usuario de un post - [HU5] #11

--- src/room.js
+++ src/room.js
@@ -1,13 +1,20 @@
+const room_type = { 
+    HABITACION: 'habitacion', 
+    VIVIENDA: 'vivienda'
+}
+Object.freeze(room_type)
+
+
 class Room {
     
     // Constructor de la clase
-    constructor(id_room,tlfn_contact,city,price,num_room_available,type,date_created,image=null){
+    constructor(id_room,tlfn_contact,city,num_room_available,date_created,price,room_type,image=null){
         this.id_room = id_room;
         this.tlfn_contact = tlfn_contact;
         this.city = city;
         this.price = price;
         this.num_room_available = num_room_available;
-        this.type = type;
+        this.type = room_type;
         this.date_created = date_created;
         this.image = image;
     }
